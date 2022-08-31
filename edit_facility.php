a67ff1e02a05defe14dca5aed3264719fb106a95
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-08-31 11:04:03 +0200

    Setup

--- edit_facility.php
+++ edit_facility.php
@@ -9,7 +9,7 @@ require "database.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Document</title>
 </head>
 <body>
     
